e2e8363be9038627243ded463a59b203ff99d42e
Ramon Rangel Lima <ramon.lima@radixeng.com.br>
2020-11-01 14:35:14 -0300

    Implementando a criação de reposirorios

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4")
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -8,25 +9,49 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+function validateRespositoryId(req, res, next) {
+  const { id } = req.params;
+
+  if(!isUuid(id)) {
+    return res.status(400).json({ error: 'Invalid Id!' });
+  }
+
+  return next();
+}
+
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.use('/repositories/:id', validateRespositoryId);
+
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
+});
+
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return res.json(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.delete("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
